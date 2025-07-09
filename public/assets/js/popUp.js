b89152df687f50bef240b448ac91bd70bb668efd
Pedro Henrique Venâncio <160691306+phvenancio@users.noreply.github.com>
2025-07-09 17:53:25 -0300

    Delete public directory

--- public/assets/js/popUp.js
+++
@@ -1,6 +0,0 @@
-function toggle() {
-  var blur = document.getElementById("blur");
-  blur.classList.toggle("active");
-  var alerta = document.getElementById("alerta");
-  alerta.classList.toggle("active");
-}
