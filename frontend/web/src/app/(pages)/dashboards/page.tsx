f392e65f5859b5c01a22c9581f2d52067da44639
DSM4 <fatec-dsm4@academico.intra>
2025-10-28 22:38:58 -0300

    feat: completa e tela de dashboard

--- frontend/web/src/app/(pages)/dashboards/page.tsx
+++ frontend/web/src/app/(pages)/dashboards/page.tsx
@@ -26,19 +26,19 @@ const Dashboards: React.FC<DashboardsProps> = ({ harvests = [] }) => {
       <h1>Dashboard</h1>
 
       <div className="graphics">
-        <div style={{ width: "100%", height: "300px" }}>
+        <div className="dash" style={{ width: "100%", height: "300px" }}>
           <DashboardChart type="bar" data={qualityData} title="Qualidade de Colheita" />
         </div>
 
-        <div style={{ width: "100%", height: "300px" }}>
+        <div className="dash" style={{ width: "100%", height: "300px" }}>
           <DashboardChart type="line" data={weeklyHarvestData} title="Colheita da Semana" />
         </div>
 
-        <div style={{ width: "100%", height: "300px" }}>
+        <div className="dash" style={{ width: "100%", height: "300px" }}>
           <DashboardChart type="line" data={growthData} title="Tendência Crescimento" />
         </div>
 
-        <div style={{ width: "100%", height: "300px" }}>
+        <div className="dash" style={{ width: "100%", height: "300px" }}>
           <DashboardChart type="pie" data={totalHarvestData} title="Total Colhido" />
         </div>
       </div>
--- frontend/web/src/app/(pages)/harvesters/page.tsx
+++ frontend/web/src/app/(pages)/harvesters/page.tsx
@@ -7,131 +7,144 @@ import "./Harvester.css";
 const Harvester: React.FC = () => {
   return (
     <>
-      <h1 className="txt">Configurações do Braço Mecânico</h1>
+      <h1>Configurações do Braço Mecânico</h1>
       {/* Camêra */}
+      
       <div className="camera">
         {/* Visor da Câmera */}
-        <video className="visor" id="video" autoPlay></video>
+        <video className="video" id="video" autoPlay></video>
       </div>
-      <div className=" column">
+      <div className="column">
         <canvas className="is-hidden" id="canvas"></canvas>
       </div>
 
-      <div className="areaBraco">
+      <div className="harvester-area">
         {/* Controles da Câmera */}
         <div id="controls">
-          <p className="btnTxt">Botões</p>
+          <p className="txt-button">Botões</p>
           <button id="take-photo">
-            <Image
-              className="botoes"
-              src="/images/icons/camera (1).png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Foto</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/icons/camera (1).png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">Foto</p>
           </button>
           <button id="start-recording">
-            <Image
-              className="botoes"
-              src="/images/icons/camera-de-video.png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Video</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/icons/camera-de-video.png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">Video</p>
           </button>
           <button id="stop-recording" disabled>
-            <Image
-              className="botoes"
-              src="/images/icons/pare.png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Parar</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/icons/pare.png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">Parar</p>
           </button>
           <button id="toggle-fullscreen">
-            <Image
-              className="botoes2"
-              src="/images/icons/expandir.png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Expandir</p>
+              <div className="button">
+                <Image
+                  className="button"
+                  src="/images/icons/expandir.png"
+                  alt=""
+                  fill
+                />
+            </div>
+            <p className="txt-icon">Expandir</p>
           </button>
         </div>
 
-        <div className="areaBraco2">
+        <div className="harvester-area">
           <button>
-            <Image
-              className="botoes"
-              src="/images/icons/relampago.png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Flash</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/icons/relampago.png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">Flash</p>
           </button>
           <button>
-            <Image
-              className="botoes"
-              src="/images/icons/sem-flash.png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">Sem Flash</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/icons/sem-flash.png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">Sem Flash</p>
           </button>
           <button>
-            <Image
-              className="botoes"
-              src="/images/mais-zoom (1).png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">+Zoom</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/mais-zoom (1).png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">+Zoom</p>
           </button>
           <button>
-            <Image
-              className="botoes"
-              src="/images/reduzir-o-zoom (1).png"
-              alt=""
-              width={40}
-              height={40}
-            />
-            <p className="txticons">-Zoom</p>
+            <div className="button">
+              <Image
+                className="button"
+                src="/images/reduzir-o-zoom (1).png"
+                alt=""
+                fill
+              />
+            </div>
+            <p className="txt-icon">-Zoom</p>
           </button>
         </div>
-        <a className="txtgaleria" href="/profile">
+        <a className="txt-gallery" href="/profile">
           Visualizar Registros de Fotos
         </a>
       </div>
 
-      <div className="mapa">
+      <div className="map">
         <iframe
-          className="areaMapa"
+          className="map-area"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58104.267122994366!2d-47.850728627694984!3d-24.467547991622677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c53436f4f0c1cb%3A0xbab60b5cc9451e73!2sRegistro%2C%20SP%2C%2011900-000!5e0!3m2!1spt-BR!2sbr!4v1729989897108!5m2!1spt-BR!2sbr"
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
 
-        <div className="btnsMapa">
-          <button className="txtMapa">Adicionar Sensores</button>
+        <div className="map-button">
+          <button className="btn btn-primary">Adicionar Sensores</button>
         </div>
-        <div className="btnsMapa2">
-          <button className="txtMapa">Ativar Alertas</button>
+        <div className="map-button">
+          <button className="btn btn-primary">Ativar Alertas</button>
         </div>
       </div>
 
-      <button className="btnVelocidade">
-        <Image src="/images/icons/btnMaior.png" alt="" width={90} height={40} />
+      <button className="btn-speed">
+        <div className="btn-big">
+          <Image src="/images/icons/btnMaior.png" alt="" fill />
+        </div>
       </button>
-      <button className="btnVelocidadeM">
-        <Image src="/images/icons/btnMenor.png" alt="" width={90} height={40} />
+      <button className="btn-speed">
+        <div className="btn-small">
+          <Image src="/images/icons/btnMenor.png" alt="" fill />
+        </div>
       </button>
 
       <div className="remote">
@@ -142,55 +155,59 @@ const Harvester: React.FC = () => {
         <div className="direction-buttons">
           <button className="direction-button up">
             {" "}
-            <Image
-              src="/images/icons/btnCima.png"
-              alt=""
-              width={43}
-              height={40}
-            />
+            <div className="btn-up">
+              <Image
+                src="/images/icons/btnCima.png"
+                alt=""
+                fill
+              />
+            </div>
           </button>
           <button className="direction-button down">
-            <Image
-              src="/images/icons/btnBaixo.png"
-              alt=""
-              width={43}
-              height={40}
-            />
+            <div className="btn-down">
+              <Image
+                src="/images/icons/btnBaixo.png"
+                alt=""
+                fill
+              />
+            </div>
           </button>
           <button className="direction-button left">
-            <Image
-              src="/images/icons/btnEsquerda.png"
-              alt=""
-              width={33}
-              height={40}
-            />
+            <div className="btn-left">
+              <Image
+                src="/images/icons/btnEsquerda.png"
+                alt=""
+                fill
+              />
+            </div>
           </button>
           <button className="direction-button right">
-            <Image
-              src="/images/icons/btnDireita.png"
-              alt=""
-              width={33}
-              height={40}
-            />
+            <div className="btn-right">
+              <Image
+                src="/images/icons/btnDireita.png"
+                alt=""
+                fill
+              />
+            </div>
           </button>
         </div>
       </div>
 
-      <div className="areaGarra">
-        <Image
-          className="direcaoGarra"
-          src="/images/icons/direcao.png"
-          alt=""
-          width={40}
-          height={40}
-        />
-        <Image
-          className="garra"
-          src="/images/icons/garra.png"
-          alt=""
-          width={40}
-          height={40}
-        />
+      <div className="harvester-area">
+        <div className="harvester-direction">
+          <Image
+            src="/images/icons/direcao.png"
+            alt=""
+            fill
+          />
+        </div>
+        <div className="harvester">
+          <Image
+            src="/images/icons/garra.png"
+            alt=""
+            fill
+          />
+        </div>
       </div>
     </>
   );
