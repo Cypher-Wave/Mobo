8435b252b25479c2abf7fb528044d9fe7eef8520
phvenancio <ph.bi.cle@gmail.com>
2026-04-28 21:40:07 -0300

    fix: url da api

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:5000/api/:path*", // proxy de API
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // proxy de API
       },
     ];
   },
