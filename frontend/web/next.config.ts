369bfabb0d97c6ede48255191dd4c7e81d6f90f7
phvenancio <ph.bi.cle@gmail.com>
2026-05-06 19:42:06 -0300

    teste

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`, // proxy de API
+        destination: `${process.env.API_URL}/:path*`, // proxy de API
       },
     ];
   },
