d057766658b563f39c694b522391b55121fbcd10
phvenancio <ph.bi.cle@gmail.com>
2026-05-07 01:54:01 -0300

    fix: agora vai 🙏🙌

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`, // proxy de API
+      },
+    ];
+  },
+
   images: {
+    unoptimized: true,
     domains: ["openweathermap.org"],
     remotePatterns: [
       {
